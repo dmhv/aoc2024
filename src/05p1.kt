4681e823ba8318c23c810b445d731d2950fc86bb
dmh <dmelichov@gmail.com>
2024-12-05 11:15:30 +0100

    Clean up

--- src/05p1.kt
+++ src/05p1.kt
@@ -1,45 +1,22 @@
 fun main() {
     val input = readInput("05")
-
     val (goesAfter, updates) = parseInput(input)
+    val correctUpdates = updates.filter {isUpdateCorrect(it, goesAfter)}
+    correctUpdates.sumOf { it[it.size / 2] }.println() // 4185
+}
 
-    val correctUpdates = mutableListOf<List<Int>>()
-
-    updates.forEach { update ->
-        val seen = mutableSetOf<Int>()
-        var isCorrect = true
-        for ((i, x) in update.withIndex()) {
-            if (!isCorrect) {
-                break
-            }
-            // check "y is after x for each y in goesAfter[x]"
-            val shouldBeAfterX = goesAfter.getOrDefault(x, setOf())
-            for (y in shouldBeAfterX) {
-                if (seen.contains(y)) {
-                    isCorrect = false
-//                    println("$update is NOT correct, as $y came before $x")
-                    break
-                }
-            }
-            // check "x is after each z for z in seen"
-            for (z in seen) {
-                val shouldBeAfterZ = goesAfter.getOrDefault(z, setOf())
-                if (!shouldBeAfterZ.contains(x)) {
-                    isCorrect = false
-//                    println("$update is NOT correct, as $x does not follow $z")
-                }
-            }
-
-            seen.add(x)
-        }
-        if (isCorrect) {
-            correctUpdates.add(update)
-        }
+private fun isUpdateCorrect(
+    update: List<Int>,
+    goesAfter: Map<Int, Set<Int>>
+): Boolean {
+    val seen = mutableSetOf<Int>()
+    return update.all { x ->
+        val shouldBeAfterX = goesAfter.getOrDefault(x, emptySet())
+        val thisIsFine = shouldBeAfterX.none { it in seen } &&
+                seen.all { z -> x in goesAfter.getOrDefault(z, emptySet()) }
+        seen.add(x)
+        thisIsFine
     }
-
-//    println("Got ${correctUpdates.size} correct updates")
-
-    correctUpdates.sumOf { it[it.size / 2] }.println()
 }
 
 private fun parseInput(input: List<String>): Pair<MutableMap<Int, Set<Int>>, MutableList<List<Int>>> {
