4abb0dd010decbe1a074e945ab9d42a97b85a6bd
dmh <dmelichov@gmail.com>
2024-12-05 19:41:13 +0100

    Clean up

--- src/04p2.kt
+++ src/04p2.kt
@@ -12,10 +12,12 @@ fun main() {
     val nCol = ls.keys.maxOf { it.second }
 
     ls.filter {
+        // find all As except at the border of the rectangle
         it.value == 'A'
         && it.key.first > 0 && it.key.first < nRow
         && it.key.second > 0 && it.key.second < nCol
     }.map { (key, _) ->
+        // grab the elements at the diagonals centered at the current A
         val (i, j) = key
         val d1 = "${ls[Pair(i - 1, j - 1)]}${ls[Pair(i + 1, j + 1)]}"
         val d2 = "${ls[Pair(i - 1, j + 1)]}${ls[Pair(i + 1, j - 1)]}"
