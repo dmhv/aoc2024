0786ab0427e1c2ebb63b36997e0366368074d21f
dmh <dmelichov@gmail.com>
2024-12-05 19:37:46 +0100

    Clean up

--- src/04p1.kt
+++ src/04p1.kt
@@ -4,12 +4,12 @@ private val SAMX = Regex("SAMX")
 private fun findXmas(s: String) = XMAS.findAll(s).toList().size + SAMX.findAll(s).toList().size
 
 fun main() {
-    val input = readInput("04")
-    val ls = mutableMapOf<Pair<Int, Int>, Char>()
-
-    input.forEachIndexed { i, line ->
-        line.toCharArray().forEachIndexed { j, c ->
-            ls[Pair(i, j)] = c
+    val input = readInput("04tiny")
+    val ls = buildMap {
+        input.forEachIndexed { i, line ->
+            line.toCharArray().forEachIndexed { j, c ->
+                this[Pair(i, j)] = c
+            }
         }
     }
 
@@ -22,19 +22,19 @@ fun main() {
 
     // vertical
     for (j in 0..nCol) {
-        val line = ls.filter { it.key.second == j }.values.joinToString("") { it.toString() }
+        val line = ls.filter { it.key.second == j }.values.joinToString("")
         count += findXmas(line)
     }
 
     // diagonal top-left to bottom-right
     for (k in 0..(nRow + nCol)) {
-        val line = ls.filter { it.key.first + it.key.second == k }.values.joinToString("") { it.toString() }
+        val line = ls.filter { it.key.first + it.key.second == k }.values.joinToString("")
         count += findXmas(line)
     }
 
     // diagonal top-right to bottom-left
     for (k in 0..(nRow + nCol)) {
-        val line = ls.filter { it.key.first + (nCol - it.key.second) == k }.values.joinToString("") { it.toString() }
+        val line = ls.filter { it.key.first + (nCol - it.key.second) == k }.values.joinToString("")
         count += findXmas(line)
     }
 
