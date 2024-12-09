659449c084ad0e3f5a2b2ffbf16dce1ade199aa0
Dmitrij Melichov <dmitrij.melichov@cognite.com>
2024-12-09 12:12:41 +0100

    09

---
+++ src/09p1.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val input = readInput("09")
+    val disk = parseInput(input[0])
+    var l = 0
+    var r = disk.size - 1
+    while (l < r) {
+        if (disk[l] != -1) l++ else if (disk[r] == -1) r-- else {
+            disk[l] = disk[r].also { disk[r] = disk[l] }
+        }
+    }
+    disk.filter { it != -1 }.mapIndexed { i, c -> (i * c).toLong() }.sum().println()
+}
+
+private fun parseInput(input: String): MutableList<Int> {
+    val out = mutableListOf<Int>()
+    input.forEachIndexed { i, c ->
+        out.addAll(List(c.digitToInt()) { if (i % 2 == 0) i / 2 else -1 })
+    }
+    return out
+}
---
+++ src/09p2.kt
@@ -0,0 +1,65 @@
+fun main() {
+    val input = readInput("09")
+    val blocks = parseInput(input[0])
+
+    val usedBlocks = blocks.filter { it.content != -1 }
+    for (b in usedBlocks.reversed()) {
+        val bId = blocks.indexOf(b)
+        val bStart = b.start
+        val bEnd = b.end
+        val bLen = bEnd - bStart
+        val bContent = b.content
+
+        val availableBlocks =
+            blocks.filter { it.content == -1 && it.end < bStart && (it.end - it.start) >= bLen }
+
+        if (availableBlocks.isNotEmpty()) {
+            val f = availableBlocks.first()
+            val fId = blocks.indexOf(f)
+            val fStart = f.start
+            val fEnd = f.end
+            val fLen = fEnd - fStart
+
+            // replace this block with free space
+            blocks.removeAt(bId)
+            blocks.add(bId, Block(bStart, bEnd, -1))
+
+            // replace the free block with this block + any leftover free space
+            blocks.removeAt(fId)
+            blocks.add(fId, Block(fStart, fStart + bLen, bContent))
+            if (fLen > bLen) blocks.add(fId + 1, Block(fStart + bLen + 1, fEnd, -1))
+        }
+    }
+
+    var checkSum = 0L
+    var i = 0
+    for (b in blocks) {
+        if (b.content == -1) {
+            for (foo in b.start..b.end) i++
+        } else {
+            for (foo in b.start..b.end) {
+                checkSum += b.content * i
+                i++
+            }
+        }
+    }
+    checkSum.println()
+}
+
+private fun parseInput(input: String): MutableList<Block> {
+    val out = mutableListOf<Block>()
+    var ptr = 0
+    for ((i, c) in input.withIndex()) {
+        val len = c.digitToInt()
+        if (len == 0) continue
+        val content = if (i % 2 == 0) i / 2 else -1
+        out.add(Block(ptr, ptr + len - 1, content))
+        ptr += len
+    }
+    return out
+}
+
+private data class Block(val start: Int, val end: Int, val content: Int)
+
+private fun Block.toStr() =
+    List(this.end - this.start + 1) { if (this.content != -1) this.content else "." }.joinToString("")
