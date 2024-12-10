4d80ff2df3f61f3d00e46f2d056e806cbbc5cb7e
Dmitrij Melichov <dmitrij.melichov@cognite.com>
2024-12-10 14:03:16 +0100

    Simplify a bit

--- src/09p2.kt
+++ src/09p2.kt
@@ -9,29 +9,22 @@ fun main() {
     val usedBlocks = blocks.filter { it.content != -1 }
     for (b in usedBlocks.reversed()) {
         val bId = blocks.indexOf(b)
-        val bStart = b.start
-        val bEnd = b.end
-        val bLen = bEnd - bStart
-        val bContent = b.content
 
         val availableBlocks =
-            blocks.filter { it.content == -1 && it.end < bStart && (it.end - it.start) >= bLen }
+            blocks.filter { it.content == -1 && it.end < b.start && it.len() >= b.len() }
 
         if (availableBlocks.isNotEmpty()) {
             val f = availableBlocks.first()
             val fId = blocks.indexOf(f)
-            val fStart = f.start
-            val fEnd = f.end
-            val fLen = fEnd - fStart
 
             // replace this block with free space
             blocks.removeAt(bId)
-            blocks.add(bId, Block(bStart, bEnd, -1))
+            blocks.add(bId, Block(b.start, b.end, -1))
 
             // replace the free block with this block + any leftover free space
             blocks.removeAt(fId)
-            blocks.add(fId, Block(fStart, fStart + bLen, bContent))
-            if (fLen > bLen) blocks.add(fId + 1, Block(fStart + bLen + 1, fEnd, -1))
+            blocks.add(fId, Block(f.start, f.start + b.len(), b.content))
+            if (f.len() > b.len()) blocks.add(fId + 1, Block(f.start + b.len() + 1, f.end, -1))
         }
     }
 
@@ -48,6 +41,7 @@ fun main() {
         }
     }
     checkSum.println()
+    check(checkSum == 6360363199987L)
 }
 
 private fun parseInput(input: String): MutableList<Block> {
@@ -64,6 +58,8 @@ private fun parseInput(input: String): MutableList<Block> {
 }
 
 private data class Block(val start: Int, val end: Int, val content: Int)
+
+private fun Block.len() = end - start
 
 private fun Block.toStr() =
     List(this.end - this.start + 1) { if (this.content != -1) this.content else "." }.joinToString("")
