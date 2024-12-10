7a875cf381018ad278f7eebb339477afe24c7ee3
Dmitrij Melichov <dmitrij.melichov@cognite.com>
2024-12-10 13:15:09 +0100

    Simplify a bit

--- src/10p1.kt
+++ src/10p1.kt
@@ -1,44 +1,36 @@
 fun main() {
     val input = readInput("10")
     val (map, heads) = parseInput(input)
-
-    var score = 0
-    for (head in heads) {
-        val s = bfs(map, head)
-//        println("$head -> $s")
-        score += s
-    }
-    println("Score: $score")
+    heads.sumOf { bfs(map, it) }.println()
 }
 
-private fun parseInput(input: List<String>): Pair<Map<Pair<Int, Int>, Int>, List<Pair<Int, Int>>> {
-    val map = mutableMapOf<Pair<Int, Int>, Int>()
-    val heads = mutableListOf<Pair<Int, Int>>()
+private fun parseInput(input: List<String>): Pair<Map<Cell, Int>, List<Cell>> {
+    val map = mutableMapOf<Cell, Int>()
+    val heads = mutableListOf<Cell>()
     for (i in input.indices) {
         val line = input[i].toCharArray()
         for (j in input[i].indices) {
-            map[Pair(i, j)] = line[j].digitToInt()
-            if (line[j].digitToInt() == 0) heads.add(Pair(i, j))
+            map[Cell(i, j)] = line[j].digitToInt()
+            if (line[j].digitToInt() == 0) heads.add(Cell(i, j))
         }
     }
     return Pair(map, heads)
 }
 
-private fun bfs(map: Map<Pair<Int, Int>, Int>, head: Pair<Int, Int>): Int {
-    val queue = ArrayDeque<Pair<Int, Int>>()
+private fun bfs(map: Map<Cell, Int>, head: Cell): Int {
+    val queue = ArrayDeque<Cell>()
     queue.add(head)
-    val maxRow = map.maxOf { it.key.first }
-    val maxCol = map.maxOf { it.key.second }
+    val maxRow = map.maxOf { it.key.row }
+    val maxCol = map.maxOf { it.key.col }
 
-    val visited = mutableSetOf<Pair<Int, Int>>()
-    val trailEnds = mutableSetOf<Pair<Int, Int>>()
+    val visited = mutableSetOf<Cell>()
+    val trailEnds = mutableSetOf<Cell>()
     while (!queue.isEmpty()) {
         val curr = queue.removeFirst()
         for (delta in listOf(Pair(-1, 0), Pair(0, -1), Pair(1, 0), Pair(0, 1))) {
-            val next = Pair(curr.first + delta.first, curr.second + delta.second)
-            if (next.first in 0..maxRow && next.second in 0..maxCol && map[next]!! - map[curr]!! == 1) {
+            val next = Cell(curr.row + delta.first, curr.col + delta.second)
+            if (next.row in 0..maxRow && next.col in 0..maxCol && map[next]!! - map[curr]!! == 1) {
                 if (map[next]!! == 9 && !trailEnds.contains(next)) {
-//                    println("..reached $next from $head")
                     trailEnds.add(next)
                 } else {
                     if (!visited.contains(next)) {
@@ -51,3 +43,5 @@ private fun bfs(map: Map<Pair<Int, Int>, Int>, head: Pair<Int, Int>): Int {
     }
     return trailEnds.size
 }
+
+data class Cell(val row: Int, val col: Int)
--- src/10p2.kt
+++ src/10p2.kt
@@ -1,43 +1,35 @@
 fun main() {
     val input = readInput("10")
     val (map, heads) = parseInput(input)
-
-    var score = 0
-    for (head in heads) {
-        val s = bfs(map, head)
-        println("$head -> $s")
-        score += s
-    }
-    println("Score: $score")
+    heads.sumOf { bfs(map, it) }.println() // 1242
 }
 
-private fun parseInput(input: List<String>): Pair<Map<Pair<Int, Int>, Int>, List<Pair<Int, Int>>> {
-    val map = mutableMapOf<Pair<Int, Int>, Int>()
-    val heads = mutableListOf<Pair<Int, Int>>()
+private fun parseInput(input: List<String>): Pair<Map<Cell, Int>, List<Cell>> {
+    val map = mutableMapOf<Cell, Int>()
+    val heads = mutableListOf<Cell>()
     for (i in input.indices) {
         val line = input[i].toCharArray()
         for (j in input[i].indices) {
-            map[Pair(i, j)] = line[j].digitToInt()
-            if (line[j].digitToInt() == 0) heads.add(Pair(i, j))
+            map[Cell(i, j)] = line[j].digitToInt()
+            if (line[j].digitToInt() == 0) heads.add(Cell(i, j))
         }
     }
     return Pair(map, heads)
 }
 
-private fun bfs(map: Map<Pair<Int, Int>, Int>, head: Pair<Int, Int>): Int {
-    val queue = ArrayDeque<Pair<Int, Int>>()
+private fun bfs(map: Map<Cell, Int>, head: Cell): Int {
+    val queue = ArrayDeque<Cell>()
     queue.add(head)
-    val maxRow = map.maxOf { it.key.first }
-    val maxCol = map.maxOf { it.key.second }
+    val maxRow = map.maxOf { it.key.row }
+    val maxCol = map.maxOf { it.key.col }
 
-    val trailEnds = mutableMapOf<Pair<Int, Int>, Int>()
+    val trailEnds = mutableMapOf<Cell, Int>()
     while (!queue.isEmpty()) {
         val curr = queue.removeFirst()
         for (delta in listOf(Pair(-1, 0), Pair(0, -1), Pair(1, 0), Pair(0, 1))) {
-            val next = Pair(curr.first + delta.first, curr.second + delta.second)
-            if (next.first in 0..maxRow && next.second in 0..maxCol && map[next]!! - map[curr]!! == 1) {
+            val next = Cell(curr.row + delta.first, curr.col + delta.second)
+            if (next.row in 0..maxRow && next.col in 0..maxCol && map[next]!! - map[curr]!! == 1) {
                 if (map[next]!! == 9) {
-//                    println("..reached $next from $head")
                     if (!trailEnds.containsKey(next)) {
                         trailEnds[next] = 1
                     } else {
