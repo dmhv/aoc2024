9db0dfc692320d7312ba5145a65b238a63e3b677
dmh <dmelichov@gmail.com>
2024-12-11 21:18:06 +0100

    Simplify

--- src/11p2.kt
+++ src/11p2.kt
@@ -8,9 +8,11 @@ fun main() {
     // https://arrow-kt.io/learn/collections-functions/recursive/#memoized-recursive-functions
     val memoCountStones = MemoizedDeepRecursiveFunction<Pair<Long, Int>, Long> { pair ->
         val (stone, steps) = pair
-        if (steps == 0) return@MemoizedDeepRecursiveFunction 1L
-        if (stone == 0L) callRecursive(Pair(1L, steps - 1))
-        if (stone.toString().length % 2 == 0) {
+        if (steps == 0) {
+            1L
+        } else if (stone == 0L) {
+            callRecursive(Pair(1L, steps - 1))
+        } else if (stone.toString().length % 2 == 0) {
             val left = stone.toString().substring(0, stone.toString().length / 2).toLong()
             val right = stone.toString().substring(stone.toString().length / 2).toLong()
             callRecursive(Pair(left, steps - 1)) + callRecursive(Pair(right, steps - 1))
