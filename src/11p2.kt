719b8a4928c8ac1cd364ac0d62dee33108e925bd
dmh <dmelichov@gmail.com>
2024-12-11 22:47:03 +0100

    Fix p2

--- src/11p2.kt
+++ src/11p2.kt
@@ -1,31 +1,26 @@
-import arrow.core.MemoizedDeepRecursiveFunction
-
-
 fun main() {
     val input = readInput("11")
     val stones = input[0].split(" ").map { it.toLong() }
 
-    // https://arrow-kt.io/learn/collections-functions/recursive/#memoized-recursive-functions
-    val memoCountStones = MemoizedDeepRecursiveFunction<Pair<Long, Int>, Long> { pair ->
-        val (stone, steps) = pair
+    val cache = mutableMapOf<Pair<Long, Int>, Long>()
+    fun countStones(stone: Long, steps: Int): Long = cache.getOrPut(Pair(stone, steps)) {
         if (steps == 0) {
             1L
         } else if (stone == 0L) {
-            callRecursive(Pair(1L, steps - 1))
+            countStones(1L, steps - 1)
         } else if (stone.toString().length % 2 == 0) {
             val left = stone.toString().substring(0, stone.toString().length / 2).toLong()
             val right = stone.toString().substring(stone.toString().length / 2).toLong()
-            callRecursive(Pair(left, steps - 1)) + callRecursive(Pair(right, steps - 1))
+            countStones(left, steps - 1) + countStones(right, steps - 1)
         } else {
-            callRecursive(Pair(stone * 2024L, steps - 1))
+            countStones(stone * 2024L, steps - 1)
         }
     }
 
     var res = 0L
-    for (stone in stones) {
-        val key = Pair(stone, 75)
-        val thisRes = memoCountStones(key)
-        println("[$stone] -> $thisRes")
+    for (num in stones) {
+        val thisRes = countStones(num, 75)
+        println("[$num] -> $thisRes")
         res += thisRes
     }
     res.println()
